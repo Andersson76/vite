de2e040cbaf4b869279400696a80a08b79d8a904
Martin Andersson <martin.andersson@me.com>
2024-11-06 14:06:38 +0100

    express

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Välkommen till Express API!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servern körs på port ${PORT}`);
+});
